06ae6408dd5f852940e9216c53b6a1ff5ccd8beb
Erick Nava <erick.nava@rutaip.net>
2016-05-27 18:49:07 +0000

    chat widget added

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -74,6 +74,19 @@
 
 </div> <!-- /container -->
 
+<script type="text/javascript">
+    var LHCChatOptions = {};
+    LHCChatOptions.opt = {widget_height:340,widget_width:300,popup_height:520,popup_width:500,domain:'phone.rutaip.net'};
+    (function() {
+        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+        var referrer = (document.referrer) ? encodeURIComponent(document.referrer.substr(document.referrer.indexOf('://')+1)) : '';
+        var location  = (document.location) ? encodeURIComponent(window.location.href.substring(window.location.protocol.length)) : '';
+        po.src = 'http://chatsoporte.rutaip.net/index.php/esp/chat/getstatus/(click)/internal/(position)/bottom_right/(ma)/br/(top)/350/(units)/pixels/(leaveamessage)/true/(department)/1?r='+referrer+'&l='+location;
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    })();
+</script>
+
+
 <footer class="footer">
     <div class="container">
         <p>
