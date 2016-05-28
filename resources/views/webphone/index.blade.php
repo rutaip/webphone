c00014e71a31098b881898190d5ed2403bc94e54
Erick Nava <erick.nava@rutaip.net>
2016-05-28 02:28:51 +0000

    softphone updated

--- resources/views/webphone/index.blade.php
+++ resources/views/webphone/index.blade.php
@@ -968,7 +968,7 @@
                                     {!! Form::text('txtPhoneNumber', null, ['class' => 'form-control', 'placeholder' => 'Enter phone number to call', 'id' => 'txtPhoneNumber']) !!}
                                 </div>
                                 <div class="col-md-4">
-                                    <input type="button" class="btn btn-block btn-default" id="btndel" value="Clean" onclick="DelDigit();"/>
+                                    <input type="button" class="btn btn-block btn-default" id="btndel" value="Clear" onclick="DelDigit();"/>
                                 </div>
                             </div>
                         </td>
@@ -1005,28 +1005,28 @@
                                     <script type="text/javascript">
 
                                         Memory = "0";      // initialise memory variable
-                                        Current = "0";      //   and value of Display ("current" value)
+                                        Current = "";      //   and value of Display ("current" value)
                                         Operation = 0;      // Records code for eg * / etc.
                                         MAXLENGTH = 30;     // maximum number of digits before decimal!
 
                                         function AddDigit(dig)          //ADD A DIGIT TO DISPLAY (kept as 'Current')
                                         {
                                             if (Current.length > MAXLENGTH) {
-                                                Current = "Aargh! Too long"; //limit length
+                                                Current = "Incorrect length"; //limit length
                                             } else {
-                                                if ((eval(Current) == 0)
-                                                        && (Current.indexOf(".") == -1)
-                                                ) {
-                                                    Current = dig;
-                                                } else {
+
                                                     Current = Current + dig;
-                                                };
+
                                             };
                                             document.getElementById("txtPhoneNumber").value = Current;
                                         };
 
                                         function DelDigit()
                                         {
+                                            Memory = "0";      // initialise memory variable
+                                            Current = "";      //   and value of Display ("current" value)
+                                            Operation = 0;      // Records code for eg * / etc.
+                                            MAXLENGTH = 30;
                                             document.getElementById("txtPhoneNumber").value = null;
                                         };
 
